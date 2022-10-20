c4d1534aeb2ed198360f86a629f2c8232bb5342c
Alejandro <juniorobregoon@gmail.com>
2022-10-19 23:28:11 -0500

    Add Funciones Integradas de Python

---
+++ 05_Built-inFunctions.py
@@ -0,0 +1,15 @@
+#Las Built-in Functions están integradas en Pyhton sin necesidad de una libreria externa
+#Probarlo en la Shell de Python
+print()
+
+#Convertir el tipo de dato
+a = int("98746")
+b = float(3)
+c = str(False)
+d = type(True)
+# help() Para saber la documentación
+
+print(a)
+print(b)
+print(c)
+print(d)
